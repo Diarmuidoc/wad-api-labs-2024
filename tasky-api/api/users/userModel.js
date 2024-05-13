83ab4d3deb42373527fe6af0bbb26375748bfc02
Diarmuid <20071587@mail.wit.ie>
2024-05-13 20:41:03 +0100

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -2,9 +2,17 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const passwordLimit = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
   password: {type: String, required: true }
 });
+
+const passwordValidator = (password) =>{
+    return password = passwordLimit.test(password);
+}
+
+UserSchema.path("password").validate(passwordValidator);
 
 export default mongoose.model('User', UserSchema);
